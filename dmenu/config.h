797524c2d010ad1ed0ea408b719c8e27982c6d00
Flohw <git@f-pa.site>
2020-10-14 00:14:15 +0200

    [dmenu] Update font size and add lineheight patch

--- dmenu/config.h
+++ dmenu/config.h
@@ -5,7 +5,7 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-        "JetBrainsMono Nerd Font:size=13"
+        "JetBrainsMono Nerd Font:size=12"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
@@ -18,6 +18,7 @@ static const char *colors[SchemeLast][2] = {
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
+static unsigned int lineheight = 25;
 
 /*
  * Characters not considered part of a word while deleting words
